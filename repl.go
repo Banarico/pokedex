543b177c4587df43ec1fd227b65bea9b3b06a596
danar <danarico10@mail.com>
2026-03-11 10:44:18 +0000

    K: added Inspect and Pokedex functions

--- repl.go
+++ repl.go
@@ -7,6 +7,7 @@ import (
     "github.com/Banarico/pokedex/internal/pokeapi"
     "github.com/Banarico/pokedex/internal/pokecache"
     "math/rand"
+    "errors"
 )
 
 func cleanInput(text string) []string {
@@ -46,6 +47,16 @@ func getCommands() map[string]cliCommand {
             name:        "catch",
             description: "Attempt to catch a given Pokemon",
             callback:    commandCatch,
+        },
+        "inspect": {
+            name:        "inspect",
+            description: "View the details of a caught Pokemon",
+            callback:    commandInspect,
+        },
+        "pokedex": {
+            name:        "pokedex",
+            description: "List all the Pokemon you've caught",
+            callback:    commandPkdx,
         },
     }
     return cmds
@@ -131,7 +142,7 @@ func commandCatch(cfg *Config, arg ...string) error {
     fmt.Printf("Throwing a Pokeball at %s...\n", arg[0])
     pkm, err := cfg.pokeapiClient.GetPokemon(arg[0])
     if err != nil {
-       fmt.Println(err)
+       return errors.New("That Pokemon doesn't exist!")
     }
     throw := rand.Intn(320)
     if throw >= pkm.BaseExp {
@@ -144,8 +155,36 @@ func commandCatch(cfg *Config, arg ...string) error {
     return nil
 }
 
+func commandInspect(cfg *Config, arg ...string) error {
+    pkm, found := cfg.caughtPkm[arg[0]]
+    if found == false {
+        return errors.New("You don't have that Pokemon!")
+    }
+    fmt.Printf("Name: %s\n", pkm.Name)
+    fmt.Printf("Height: %d\n", pkm.Height)
+    fmt.Printf("Weight: %d\n", pkm.Weight)
+    fmt.Println("Stats:")
+    for _, s := range pkm.Stats {
+        fmt.Printf("  -%s: %d\n", s.Stat.Name, s.BaseStat)
+    }
+    fmt.Println("Types:")
+    for _, t := range pkm.Types {
+        fmt.Printf("  - %s\n", t.Type.Name)
+    }
+    return nil
+}
+
+func commandPkdx(cfg *Config, arg ...string) error {
+    fmt.Println("Your Pokedex:")
+    for _, pkm := range cfg.caughtPkm {
+        fmt.Printf(" - %s\n", pkm.Name)
+    }
+    return nil
+}
+
 type cliCommand struct {
         name        string
         description string
         callback    func(*Config, ...string) error
 }
+
